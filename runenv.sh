81a67565f490c55b97b2ea1cb0d6c3c198a83efb
Jan De Landtsheer <jan@greenitglobe.com>
2018-01-15 14:21:13 +0100

    Oops, a little too many 'finished' messages

--- runenv.sh
+++ runenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # build it, you'll find the build result in ./binaries
-docker run --privileged -it \
+docker run --privileged -it --rm \
 	-v $(pwd):/911builder 911builder:latest bash
 
